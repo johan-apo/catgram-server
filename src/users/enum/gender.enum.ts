e323284bdbbd588de8b3f94259cfe80c68c76c9d
Johan Apolinario Véliz <johelapo@gmail.com>
2022-09-06 22:58:59 -0500

    Drop out TypeORM in favor of using Mongoose.

--- src/users/enum/gender.enum.ts
+++
@@ -1,6 +0,0 @@
-export enum GenderEnum {
-  Male = 'Male',
-  Female = 'Female',
-  Other = 'Other',
-  ItIsPrivate = 'Private',
-}
--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -1,15 +1,12 @@
 import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
-import { TypeOrmModule } from '@nestjs/typeorm';
 import { Post, PostSchema } from 'src/posts/schemas/post.schema';
-// import User from './entities/user.entity';
 import { User, UserSchema } from './schemas/user.schema';
 import { UsersController } from './users.controller';
 import { UsersService } from './users.service';
 
 @Module({
   imports: [
-    // TypeOrmModule.forFeature([User]),
     MongooseModule.forFeature([
       { name: User.name, schema: UserSchema },
       { name: Post.name, schema: PostSchema },
